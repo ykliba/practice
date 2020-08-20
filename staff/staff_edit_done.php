0ec412acc63a522a173c2396affc6827e310d02a
ykliba <mediaboxing2@gmail.com>
2020-08-20 16:14:48 +0900

    php

---
+++ staff/staff_edit_done.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>practice</title>
+</head> 
+<body>
+
+<?php
+
+try {
+  $staff_code = $_POST['code'];
+  $staff_name = $_POST['name'];
+  $staff_pass = $_POST['pass'];
+
+  
+  $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+  $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+  $dsn='mysql:dbname=practice;host=localhost;charset=utf8';
+  $user = 'root';
+  $password = 'root';
+  $dbh = new PDO($dsn,$user,$password);
+  $dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+  $sql = 'UPDATE staff SET name=?,password=?  WHERE code=?';
+  $stmt = $dbh -> prepare($sql);
+  $data[] = $staff_name;
+  $data[] = $staff_pass;
+  $data[] = $staff_code;
+  $stmt -> execute($data);
+
+  $dbh = null;
+}
+catch(Exception $e) {
+  // echo 'ただいま障害により大変ご迷惑をお掛けしております。';
+  echo $e->getFile(), '/', $e->getLine(), ':', $e->getMessage();
+  exit();
+}
+?>
+修正しました。<br>
+<br>
+<a href = "staff_list.php">戻る</a>;
+</body>
